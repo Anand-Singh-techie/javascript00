13c69b13ded0533ffc625825a025e63e18940395
Anand Singh <94180760+Anand-Singh-techie@users.noreply.github.com>
2022-03-22 20:41:02 +0530

    Upadte Hello.js
    I have mentioned some of the other ways to print a particular text in the console.

--- HelloWorld.js
+++ HelloWorld.js
@@ -3,3 +3,8 @@ console.log("Hello World");
 alert("Hello World");
 
 document.write("Hello World");
+
+// another way
+var text = "Hello World"
+console.log(`The text is ${text}`)
+console.log("The text is %s",text);
